73c8270478ec49adb504134306f463efcec37bd3
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2025-11-09 16:16:16 -0300

    :tada: Criado o servidor e conectado com as rotas de usuario

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pg from 'pg';
+import userRoutes from './app/routes/userRoutes.js';
+
+dotenv.config()
+const app = express();
+app.use(express.json());
+app.use('/user', userRoutes);
+
+const DOOR = process.env.DOOR || 3000
+const DB_URL = process.env.DATABASE_URL
+
+const connection = new pg.Pool({
+    connectionString: DB_URL
+})
+
+app.get('/', (req, res) => {
+    res.send("Servidor rodando!")
+})
+
+async function connectDb() {
+    try {
+        const client = await connection.connect()
+        console.log("[SUCESSO] Database conectado com sucesso ao CRUD!");
+        client.release();
+    } catch (error) {
+        console.error('[ERRO] Erro ao se conectar ao Database! ', error.message);
+        process.exit(1)
+    }
+}
+
+connectDb().then(()=>{
+    app.listen(DOOR, () => {
+        console.log(`O servidor está rodando em: http://localhost:${DOOR}`); 
+    })
+})
